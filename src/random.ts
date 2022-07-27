6bdfec5981e00b4480ba60ac669c7d71569e716b
BergerAPI <bergerbtw@gmail.com>
2022-07-27 19:35:34 +0200

    Added Functions to generate random ints and floats

---
+++ src/random.ts
@@ -0,0 +1,13 @@
+/**
+ * (Insecurely) Generates a random integer between `min` and `max`.
+ */
+export function randomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * (Insecurely) Generates a random float between `min` and `max`.
+ */
+export function randomFloat(min: number, max: number): number {
+    return Math.random() * (max - min) + min;
+}
